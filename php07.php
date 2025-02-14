816da006c99998aaebd416882127051bd41ca8ca
Agatabrete <144770128+Agatabrete@users.noreply.github.com>
2025-02-14 13:15:04 +0200

    09

--- php07.php
+++ php07.php
@@ -83,8 +83,115 @@ koosta kood, mis lehele laadimisel valib suvaliselt igast massiivist elemendi ja
         }
         
         echo '<br>';
+        ?>
 
+        <h2>Arvud</h2>
 
+        <form action="" method="get">
+                    <div class="mb-3">
+                        <label class="form-label">Sisesta esimene arv</label>
+                        <input type="number" class="form-control" name="arv1">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label
+                        ">Sisesta viimane arv</label>
+                        <input type="number" class="form-control" name="arv2">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Sammupikkus</label>
+                        <input type="number" class="form-control" name="samm">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Saada</button>
+                </form>
+        
+        <?php 
+        function arvud($arv1, $arv2, $samm) {
+        for ($arv = $arv1; $arv <= $arv2; $arv += $samm) {
+                echo "$arv";
+                echo ", ";
+            }
+        }
+                            
+        if (isset($_GET['arv1'], $_GET['arv2'], $_GET['samm'])) {
+            $arv1 = $_GET['arv1'];
+            $arv2 = $_GET['arv2'];
+            $samm = $_GET['samm'];
+                            
+            arvud($arv1, $arv2, $samm);
+        
+        }
+        echo '<br>';
+        ?>
+
+        <h2>Ristküliku pindala</h2>
+        <form action="" method="get">
+                    <div class="mb-3">
+                        <label class="form-label">Sisesta külg A</label>
+                        <input type="number" class="form-control" name="a">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Sisesta külg B</label>
+                        <input type="number" class="form-control" name="b">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Saada</button>
+                </form>
+
+        <?php
+        function pindala($a, $b){
+            $pindala = 2*($a+$b);
+            return $pindala;
+        }
+        if (isset($_GET['a'], $_GET['b'])) {
+            $a = $_GET['a'];
+            $b = $_GET['b'];
+            echo "Ristküliku pindala: ".pindala($a, $b);
+        }
+        echo '<br>';
+
+        ?>
+
+        <h2>Isikukood</h2>
+        <form action="" method="get">
+                    <div class="mb-3">
+                        <label class="form-label">Sisesta isikukood</label>
+                        <input type="number" class="form-control" name="isikukood">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Saada</button>
+                </form>
+
+        <?php
+        function isikukood($isikukood){
+        $isikukood = strval($isikukood);
+        if(strlen($isikukood) == 11){
+            $sugu = substr($isikukood, 0, 1);
+            if($sugu % 2 == 0){
+                $sugu = "Naine";
+            }else{
+                $sugu = "Mees";
+                }
+            $sunniaeg = substr($isikukood, 1, 2) . "." . substr($isikukood, 3, 2) . "." . substr($isikukood, 5, 2);
+            return "Sugu: $sugu <br> Sünniaeg: $sunniaeg";
+        }else{
+            return "Isikukood on vale pikkusega";
+        }
+        }
+        if(isset($_GET['isikukood'])){
+            echo isikukood($_GET['isikukood']);
+        }
+        echo '<br>';
+
+        echo "<h2>Head mõtted</h2>";
+
+        function motted(){
+            $alus = array("Täna", "eile", "Üleeile", "homme", "Ülehomme");
+            $oeldis = array("on", "ei taha", "tahan", "ei saa olla", "saab olla");
+            $sihitis = array("naisi", "mariot", "lund", "mehi", "õnne");
+            $sona1 = $alus[array_rand($alus)];
+            $sona2 = $oeldis[array_rand($oeldis)];
+            $sona3 = $sihitis[array_rand($sihitis)];
+            return "$sona1 $sona2 $sona3";
+            }
+        echo motted()
         ?>
 
 
