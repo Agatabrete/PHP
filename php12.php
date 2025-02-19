b6bc731337c2ab3df108453d9ee756acf9ba12da
Agatabrete <144770128+Agatabrete@users.noreply.github.com>
2025-02-19 15:21:43 +0200

    14

---
+++ php12.php
@@ -0,0 +1,21 @@
+<!-- AGATA BRETE JÕPISELGSõiduaeg – leia auto sõiduaeg tundides ja minutites. Arvutused teosta kasutajalt saadud andmete põhjal, kus kasutaja lisab stardi ja lõppaja kujul hh:mm. Eeldame, et sõiduaeg jääb ühe ööpäeva sisse. Lisa tühja välja kontroll ja näiteks, kas lisatud ajad on vähemalt viis sümbolit pikad.
+Palkade võrdlus – kasuta tootajad.csv andmefaili, kuhu on lisatud töötajate nimed, sugu ja palganumber. Koosta programm, mis analüüsib kas firmas toimub diskrimineerimist soo järgi. Too välja meeste ja naiste palkade keskmiseid ja kõige suuremad palgad. -->
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Harjutused</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Harjutus 12</h1>
+        <?php
+
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
