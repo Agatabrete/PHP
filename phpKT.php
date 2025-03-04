5b4eeb8fc46058bb723bb930fdd80bcbcf433d45
Agatabrete <144770128+Agatabrete@users.noreply.github.com>
2025-03-04 11:30:44 +0200

    KT

--- phpKT.php
+++ phpKT.php
@@ -1,3 +1,15 @@
+<!-- 10p - 5 (Ćµigeaegselt)
+8p - 4
+6p - 3
+
+Koosta Bootstrap5 abil samasugune leht 1p
+	- kohandub ja valideerub
+Koosta menĆ¼Ć¼, millel klikkides sisu muutub dĆ¼naamiliselt 1p
+	- teiste lehtede sisse pane lihtsalt pealkiri ja tekst
+	- kui lehte ei eksisteeri, siis tuleb vastav teade
+Kasuta piltide massiivi, et avalehel bĆ¤nner vahetuks suvaliselt 2p
+Postitused tulevad TXT failist (pealkiri, lĆ¼hitekst) 2p
+Loo adminni leht, kus admin saab postitusi lisada ja kustutada 4p -->
 <!DOCTYPE html>
 <html lang="et">
 <head>
@@ -12,17 +24,15 @@
         background-size: cover;
       }
     </style>
-</head>
-<body>
-<div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container jumbo">
+      <nav class="navbar navbar-expand-lg bg-transparent">
         <div class="container-fluid">
           <a class="navbar-brand text-light" href="#"> Agata Brete Jõpiselg</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon text-light"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 float-right text-right">
+            <ul class="navbar-nav mb-2 mb-lg-0 ms-auto ">
               <li class="nav-item">
                 <a class="nav-link active text-light" aria-current="page" href="#">Avaleht</a>
               </li>
@@ -38,14 +48,20 @@
             </ul>
             
           </div>
-        </div>
-    </div>
-    <div class="p-5 mb-4 bg-body-tertiary jumbo">
+        </div> 
+    </nav>   
+      <div class="p-5 mb-4 ">
       <div class="container-fluid py-5 text-center text-light">
         <h1 class="display-5 fw-bold text-center">Praktika Maltal</h1>
         <p class=" fs-4 text-center display-7">Minu välispraktika HKHKs</p>
       </div>
+      
     </div>
+    </div>
+
+</head>
+<body>
+
 
 <div class="container">
     <?php
