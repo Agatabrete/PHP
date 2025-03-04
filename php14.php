62f907c74419e22bcf9f9721570765c8e5155d50
Agatabrete <144770128+Agatabrete@users.noreply.github.com>
2025-03-04 10:28:46 +0200

    Update php14.php

--- php14.php
+++ php14.php
@@ -1,71 +1,88 @@
-<!-- AGATA BRETE JÕPISELG Suvaline pilt – koosta kood, mis valib kataloogist suvalise pildi
-Pisipildid veergudes – koosta kood, mis kuvab pisipildid näiteks kolmes veerus. Piltidele klikkides kuvatakse suurem pilt ning veergude arvu saan koodis kiiresti muuta. Pisipildid võid tekitada Photoshsopi abiga. -->
-<!doctype html>
-<html lang="et">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Harjutused</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    </head>
-<body>
-    <div class="container">
-        <h1>Harjutus 14</h1>
+<?php
+$kataloog = 'img';
+$pildid = [];
 
-        <form method="post" action="">
-            <select name="pildid"> 
-        <option value="">Vali pilt</option>
-        <?php 
-            $kataloog = '14_pildid';
-            $asukoht=opendir($kataloog);
-            while($rida = readdir($asukoht)){
-                if($rida!='.' && $rida!='..'){
-                    echo "<option value='$rida'>$rida</option>\n";
-                }
-            }
-        ?>
-    </select>
-    <input type="submit" value="Vaata">
-    </form>
-    <?php
-    if(!empty($_POST['pildid'])){
-        $pilt = $_POST['pildid'];
-        $pildi_aadress = '14_pildid/'.$pilt;
-        $pildi_andmed = getimagesize($pildi_aadress);
-        
-        $laius = $pildi_andmed[0];
-        $korgus = $pildi_andmed[1];
-        $formaat = $pildi_andmed[2];
-        $max_laius = 120;
-        $max_korgus = 90;
-        
-        //suhtearvu leidmine
-        if($laius <= $max_korgus && $korgus<=$max_korgus){
-            $ratio = 1;	
-        } elseif($laius>$korgus){
-            $ratio = $max_laius/$laius;	
-        } else {
-            $ratio = $max_korgus/$korgus;	
+if (is_dir($kataloog)) {
+    $asukoht = opendir($kataloog);
+    while ($rida = readdir($asukoht)) {
+        if ($rida !== '.' && $rida !== '..') {
+            $pildid[] = $rida;
         }
-        
-        //uute mõõtmete leidmine
-        $pisi_laius = round($laius*$ratio);
-        $pisi_korgus = round($korgus*$ratio);
-        
-        echo '<h3>Originaal pildi andmed</h3>';
-        echo "Laius: $laius<br>";
-        echo "Kõrgus: $korgus<br>";
-        echo "Formaat: $formaat<br>";
-        
-        echo '<h3>Uue pildi andmed</h3>';
-        echo "Arvutamse suhe: $ratio <br>";
-        echo "Laius: $pisi_laius<br>";
-        echo "Kõrgus: $pisi_korgus<br>";
-        echo "<img width='$pisi_laius' src='$pildi_aadress'><br>";
     }
-    ?>
+    closedir($asukoht);
+}
+
+$suvapildi_suurus = 400;
+$pisi_laius = 120;
+$pisi_korgus = 90;
+$veergudearv = 16;
+
+
+function pildisuurus($pildi_aadress, $max_laius, $max_korgus) {
+    global $laius, $korgus;
+    $pildi_andmed = getimagesize($pildi_aadress);
+
+    $laius = $pildi_andmed[0];
+    $korgus = $pildi_andmed[1];
+
+    if ($laius <= $max_laius && $korgus <= $max_korgus) {
+        $ratio = 1;
+    } elseif ($laius > $korgus) {
+        $ratio = $max_laius / $laius;
+    } else {
+        $ratio = $max_korgus / $korgus;
+    }
+
+    $laius = round($laius * $ratio);
+    $korgus = round($korgus * $ratio);
+}
+
+$suvapilt = $pildid[array_rand($pildid)];
+$suva_asukoht = "$kataloog/$suvapilt";
+pildisuurus($suva_asukoht, $suvapildi_suurus, $suvapildi_suurus);
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Harjutus14</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container text-center mt-4">
+
+        <h1 class="mb-4">Suvaline pilt</h1>
+        
+        <img src="<?php echo $suva_asukoht; ?>" alt="Suvaline pilt" class="img-fluid mb-4" width="<?php echo $laius; ?>" height="<?php echo $korgus; ?>">
+
+        <h1>Suvalised pisipildid</h1>
+        <div class="row mt-5 mb-5">
+
+            <?php 
+            $piltide_arv = 0;
+            do {
+                $suvalinePisipilt = $pildid[array_rand($pildid)];
+                $pisipildiTee = "$kataloog/$suvalinePisipilt";
+                pildisuurus($pisipildiTee, $pisi_laius, $pisi_korgus);
+            ?>
+
+            <div class="col-md-3 col-sm-4 col-6 mb-3">
+                <a href="<?php echo $pisipildiTee; ?>" target="_blank"><img src="<?php echo $pisipildiTee; ?>" alt="Pisipilt" class="img-fluid" width="<?php echo $laius; ?>" height="<?php echo $korgus; ?>"></a>
+            </div>
+
+            <?php 
+                $piltide_arv++;
+            } while ($piltide_arv < $veergudearv); 
+            ?>
+        </div>
+        
+        <button onclick="location.reload();" class="btn btn-primary mt-4 mb-5">Uuenda pilte</button>
+
+
+
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
